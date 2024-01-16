bc83e592dc259307f9f76ea389e7bea45628a7dd
Nikhilnv1209 <nikhilverma5000.nv@gmail.com>
2024-01-17 00:15:07 +0530

    added types and backspace functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,27 @@
-import Chipcomponent from '@/components/Chipcomponent'
+import Chipcomponent from "@/components/Chipcomponent";
 
-export default function Home() {
+export interface IUserinfo {
+  id: number;
+  imageUrl: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+}
+
+const fetchData = async () => {
+  const res = await fetch(
+    "https://hub.dummyapis.com/employee?noofRecords=15&idStarts=1"
+  );
+  const data = await res.json();
+  return data;
+};
+
+export default async function Home() {
+  const userinfo: IUserinfo[] = await fetchData();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <Chipcomponent />
+    <main className="flex min-h-screen flex-col items-center gap-10 p-24">
+      <h1>User Picker</h1>
+      <Chipcomponent userinfo={userinfo} />
     </main>
-  )
+  );
 }
--- components/Chipcomponent.tsx
+++ components/Chipcomponent.tsx
@@ -1,85 +1,110 @@
 "use client";
-import { useRef, useState } from "react";
-import { emails } from "@/constants/emails";
+import { KeyboardEvent, useEffect, useState } from "react";
+import { IUserinfo } from "@/app/page";
+import Image from "next/image";
 
-function Chipcomponent() {
-  const [Emails, setEmails] = useState<string[]>(emails);
-  const [selectedEmails, setSelectedEmails] = useState<string[]>([]);
-  const [inputValue, setInputValue] = useState<string>("");
+function Chipcomponent({ userinfo }: { userinfo: IUserinfo[] }) {
+  const [localUserinfo, setLocalUserinfo] = useState<IUserinfo[]>([]);
+  const [selectedUsers, setSelectedUsers] = useState<IUserinfo[]>([]);
+  const [inputText, setInputText] = useState<string>("");
   const [isInputFocused, setIsInputFocused] = useState<boolean>(false);
-  const inputRef = useRef<HTMLInputElement>(null);
+  const [userhighlight, setUserhighlight] = useState<number | null>(null);
 
-  const handleInputClick = (email: string) => {
-    if(selectedEmails.includes(email)) return;
-    const newSelectedEmails = [...selectedEmails, email];
-    const newemails = emails.filter((email) => !newSelectedEmails.includes(email));
-    setSelectedEmails(newSelectedEmails);
-    setEmails(newemails);
-    setInputValue("");
+  // Sync local userinfo state with the prop
+  useEffect(() => {
+    setLocalUserinfo(userinfo);
+  }, [userinfo]);
+
+  const handleUserSelect = (selectedUser: IUserinfo) => {
+    setSelectedUsers([...selectedUsers, selectedUser]);
+    setLocalUserinfo(
+      localUserinfo.filter((user) => user.id !== selectedUser.id)
+    );
+    setInputText("");
     setIsInputFocused(false);
   };
 
-  const handleSelectedEmails = (email: string) => {
-    const newSelectedEmails = selectedEmails.filter((selectedEmail) => selectedEmail !== email);
-    const newemails = [...emails, email];
-    setSelectedEmails(newSelectedEmails);
-    setEmails(newemails);
-  }
+  const handleUserRemove = (selectedUser: IUserinfo) => {
+    setSelectedUsers(
+      selectedUsers.filter((user) => user.id !== selectedUser.id)
+    );
+    setLocalUserinfo([...localUserinfo, selectedUser]);
+    setInputText("");
+    setIsInputFocused(true);
+  };
+
+  const keydownHandler = (event: KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Backspace" && inputText === "") {
+      if (userhighlight === null) {
+        setUserhighlight(selectedUsers.length - 1);
+      } else {
+        setSelectedUsers(selectedUsers.slice(0, -1));
+        setLocalUserinfo([
+          ...localUserinfo,
+          selectedUsers[selectedUsers.length - 1],
+        ]);
+        setUserhighlight(null);
+      }
+    } 
+  };
 
   return (
-    <div className="w-[40%] max-w-[40%] p-1 border border-black min-h-10">
+    <div className="w-[40vw] max-w-[40vw] p-1 border-b-2 border-blue-500 min-h-10">
       <span className="flex gap-1 flex-wrap">
-        {selectedEmails.length > 0 &&
-          selectedEmails.map((email, index) => {
-            return (
-              <span
-                key={email + index}
-                className="bg-black/40 h-8 px-3 flex gap-2 items-center text-white text-sm rounded-full cursor-pointer"
-                onClick={() => {handleSelectedEmails(email)}}
-              >
-                <p>{email}</p>
-                <span>x</span>
-              </span>
-            );
-          })}
-        {/* input for the emails */}
+        {selectedUsers.map((user, index) => (
+          <span
+            key={user.id}
+            className={`bg-[#393E46] h-10 px-3 flex gap-2 items-center text-white text-sm rounded-xl cursor-pointer border-2 justify-center ${
+              userhighlight === index ? "border-blue-600" : ""
+            }`}
+            onClick={() => handleUserRemove(user)}
+          >
+            <span className="h-6 relative w-6 inline-block">
+              <Image
+                src={user.imageUrl}
+                alt="profileimage"
+                fill
+                className="rounded-xl"
+              />
+            </span>
+            <p>{user.firstName}</p>
+            <span>x</span>
+          </span>
+        ))}
         <div className="relative flex items-center max-w-[40%]">
           <input
             className="border-none outline-none bg-inherit relative w-full"
-            onChange={(e) => setInputValue(e.target.value)}
+            value={inputText}
+            onChange={(e) => setInputText(e.target.value)}
             onFocus={() => setIsInputFocused(true)}
+            onKeyDown={keydownHandler}
           />
           {isInputFocused && (
-            <div className="flex flex-col max-h-40 absolute top-[105%] left-0 gap-2 bg-white shadow-md overflow-y-auto overflow-x-hidden">
-              {Emails.length > 0 &&
-                Emails
-                  .filter((email) => email.includes(inputValue))
-                  .map((email, index) => {
-                    return (
-                      <span
-                        key={email + index}
-                        className="h-8 px-3 flex gap-2 items-center text-black cursor-pointer"
-                        onClick={() => handleInputClick(email)}
-                      >
-                        <p>{email}</p>
-                      </span>
-                    );
-                  })}
+            <div className="flex flex-col max-h-64 absolute top-[105%] left-0 gap-2 bg-[#EEEEEE] rounded-md text-sm shadow-md overflow-y-auto overflow-x-hidden py-2 scrollbar-thin scrollbar-thumb-[#393E46] scrollbar-track-[#EEEEEE] scrollbar-thumb-rounded-md">
+              {localUserinfo
+                .filter((user) =>
+                  user.email.toLowerCase().includes(inputText.toLowerCase())
+                )
+                .map((user) => (
+                  <span
+                    key={user.id}
+                    className="h-8 px-2 flex gap-2 items-center text-black cursor-pointer"
+                    onClick={() => handleUserSelect(user)}
+                  >
+                    <span className="h-7 relative w-7 inline-block">
+                      <Image
+                        src={user.imageUrl}
+                        alt="profileimage"
+                        fill
+                        className="rounded-full"
+                      />
+                    </span>
+                    <p>{user.email}</p>
+                  </span>
+                ))}
             </div>
           )}
         </div>
-        {/* <span className='bg-black/40 h-8 px-3 flex items-center text-white'>
-        <p>hello this is an email</p>
-      </span>
-      <span className='bg-black/40 h-8 px-3 flex items-center text-white'>
-        <p>hello this is an email</p>
-      </span>
-      <span className='bg-black/40 h-8 px-3 flex items-center text-white'>
-        <p>hello this is an email</p>
-      </span>
-      <span className='bg-black/40 h-8 px-3 flex items-center text-white'>
-        <p>hello this is an email</p>
-      </span> */}
       </span>
     </div>
   );
